7d9cce6fa2d5613720e615708a0a77b4595185bd
svn <svn@e079088b-210c-0410-b4e2-8a6df4aeee23>
2006-04-14 18:47:22 +0000

    Added more tests.

--- contract4j5/test/org/contract4j5/test/ContractInterfaceTest.java
+++ contract4j5/test/org/contract4j5/test/ContractInterfaceTest.java
@@ -3,17 +3,23 @@ package org.contract4j5.test;
 import junit.framework.TestCase;
 
 import org.contract4j5.ContractError;
+import org.contract4j5.Invar;
+import org.contract4j5.Post;
+import org.contract4j5.Pre;
 
 /** 
  * Tests and demonstrates behavior of a class that implements a contract-defining
- * Interface, without any annotations in the class. The method tests are not 
- * inherited unless the annotations are used. No tests fail that should fail 
- * even though the contract is violated.
+ * Interface, with or without any annotations in the class. 
+ * For "ContractInterfaceImplWithNoAnnos", The method tests are not inherited 
+ * because the annotations are not used. No tests fail that should fail even 
+ * though the contract is violated.
+ * In contrast, "ContractInterfaceImplWithAnnos" carries the annnotations, so
+ * the contracts fails when they should fail.
  * @author Dean Wampler <mailto:dean@aspectprogramming.com>
  */
 public class ContractInterfaceTest extends TestCase {
 
-	public static class ContractInterfaceSimpleImpl implements ContractInterface {
+	public static class ContractInterfaceImplWithNoAnnos implements ContractInterface {
 		private String name = null;
 		public void setName (String s) { name = s; }
 		public String getName () { return name; }
@@ -22,19 +28,31 @@ public class ContractInterfaceTest extends TestCase {
 		public int getFlag() { return 0; }
 	}
 		
-	private ContractInterfaceSimpleImpl impl = null;
+	private ContractInterfaceImplWithNoAnnos implWithNoAnnos = null;
+	
+	public static class ContractInterfaceImplWithAnnos implements ContractInterface {
+		private String name = null;
+		@Pre   public void setName (String s) { name = s; }
+		@Post  public String getName () { return name; }
+		@Invar public void m (String s) {}
+		public boolean getB() {	return true; }
+		public int getFlag() { return 0; }
+	}
+		
+	private ContractInterfaceImplWithAnnos implWithAnnos = null;
 	
 	protected void setUp() throws Exception {
 		super.setUp();
-		impl = new ContractInterfaceSimpleImpl();
+		implWithNoAnnos = new ContractInterfaceImplWithNoAnnos();
+		implWithAnnos   = new ContractInterfaceImplWithAnnos();
 	}
 
 	/*
 	 * Test method for 'org.contract4j5.test.ContractInterfaceImpl.setName(String)'
 	 */
-	public void testSetNameWithNull() {
+	public void testNoAnnosSetNameWithNull() {
 		try {
-			impl.setName(null);  // Breaks contract!
+			implWithNoAnnos.setName(null);  // Breaks contract!
 		} catch (ContractError e) {
 			fail();
 		}
@@ -43,9 +61,9 @@ public class ContractInterfaceTest extends TestCase {
 	/*
 	 * Test method for 'org.contract4j5.test.ContractInterfaceImpl.setName(String)'
 	 */
-	public void testSetNameWithNonNull() {
+	public void testNoAnnosSetNameWithNonNull() {
 		try {
-			impl.setName("ok");
+			implWithNoAnnos.setName("ok");
 		} catch (ContractError e) {
 			fail();
 		}
@@ -54,9 +72,9 @@ public class ContractInterfaceTest extends TestCase {
 	/*
 	 * Test method for 'org.contract4j5.test.ContractInterfaceImpl.getName()'
 	 */
-	public void testGetNameWithNull() {
+	public void testNoAnnosGetNameWithNull() {
 		try {
-			impl.getName(); // Breaks contract!
+			implWithNoAnnos.getName(); // Breaks contract!
 		} catch (ContractError e) {
 			fail();
 		}
@@ -65,10 +83,10 @@ public class ContractInterfaceTest extends TestCase {
 	/*
 	 * Test method for 'org.contract4j5.test.ContractInterfaceImpl.getName()'
 	 */
-	public void testGetNameWithNonNull() {
+	public void tesNoAnnostGetNameWithNonNull() {
 		try {
-			impl.setName("ok");
-			impl.getName();
+			implWithNoAnnos.setName("ok");
+			implWithNoAnnos.getName();
 		} catch (ContractError e) {
 			fail();
 		}
@@ -77,9 +95,9 @@ public class ContractInterfaceTest extends TestCase {
 	/*
 	 * Test method for 'org.contract4j5.test.ContractInterfaceImpl.m(String)'
 	 */
-	public void testMWithNullName() {
+	public void testNoAnnosMWithNullName() {
 		try {
-			impl.m("toss"); // Breaks contract because name never initialized!
+			implWithNoAnnos.m("toss"); // Breaks contract because name never initialized!
 		} catch (ContractError e) {
 			fail();
 		}
@@ -88,10 +106,106 @@ public class ContractInterfaceTest extends TestCase {
 	/*
 	 * Test method for 'org.contract4j5.test.ContractInterfaceImpl.m(String)'
 	 */
-	public void testMWithEmptyName() {
+	public void testNoAnnosMWithEmptyName() {
 		try {
-			impl.setName("");
-			impl.m("toss"); // Breaks contract because name is empty!
+			implWithNoAnnos.setName("");
+			implWithNoAnnos.m("toss"); // Breaks contract because name is empty!
+		} catch (ContractError e) {
+			fail();
+		}
+	}
+
+	/*
+	 * Test method for 'org.contract4j5.test.ContractInterfaceImpl.m(String)'
+	 */
+	public void testNoAnnosMWithNonEmptyName() {
+		try {
+			implWithNoAnnos.setName("foo");
+			implWithNoAnnos.m("toss"); 
+		} catch (ContractError e) {
+			fail();
+		}
+	}
+
+	/*
+	 * Test method for 'org.contract4j5.test.ContractInterfaceImpl.setName(String)'
+	 */
+	public void testWithAnnosSetNameWithNull() {
+		try {
+			implWithAnnos.setName(null);  // Breaks contract!
+			fail();
+		} catch (ContractError e) {
+			// Expected
+		}
+	}
+
+	/*
+	 * Test method for 'org.contract4j5.test.ContractInterfaceImpl.setName(String)'
+	 */
+	public void testWithAnnosSetNameWithNonNull() {
+		try {
+			implWithAnnos.setName("ok");
+		} catch (ContractError e) {
+			fail();
+		}
+	}
+
+	/*
+	 * Test method for 'org.contract4j5.test.ContractInterfaceImpl.getName()'
+	 */
+	public void testWithAnnosGetNameWithNull() {
+		try {
+			implWithAnnos.getName(); // Breaks contract!
+			fail();
+		} catch (ContractError e) {
+			// Expected
+		}
+	}
+
+	/*
+	 * Test method for 'org.contract4j5.test.ContractInterfaceImpl.getName()'
+	 */
+	public void testWithAnnosGetNameWithNonNull() {
+		try {
+			implWithAnnos.setName("ok");
+			implWithAnnos.getName();
+		} catch (ContractError e) {
+			fail();
+		}
+	}
+
+	/*
+	 * Test method for 'org.contract4j5.test.ContractInterfaceImpl.m(String)'
+	 */
+	public void testWithAnnosMWithNullName() {
+		try {
+			implWithAnnos.m("toss"); // Breaks contract because name never initialized!
+			fail();
+		} catch (ContractError e) {
+			// Expected
+		}
+	}
+
+	/*
+	 * Test method for 'org.contract4j5.test.ContractInterfaceImpl.m(String)'
+	 */
+	public void testWithAnnosMWithEmptyName() {
+		try {
+			implWithAnnos.setName("");
+			implWithAnnos.m("toss"); // Breaks contract because name is empty!
+			fail();
+		} catch (ContractError e) {
+			// Expected
+		}
+	}
+
+	/*
+	 * Test method for 'org.contract4j5.test.ContractInterfaceImpl.m(String)'
+	 */
+	public void testWithAnnosMWithNonEmptyName() {
+		try {
+			implWithAnnos.setName("foo");
+			implWithAnnos.m("toss"); 
 		} catch (ContractError e) {
 			fail();
 		}
