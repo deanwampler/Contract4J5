0e5277cc3c4425a2ccc4ccf5924720814642e892
svn <svn@e079088b-210c-0410-b4e2-8a6df4aeee23>
2007-09-12 16:59:15 +0000

    Better handling of the query for the interpreter being used.

--- contract4j5/test/org/contract4j5/util/SystemUtils.java
+++ contract4j5/test/org/contract4j5/util/SystemUtils.java
@@ -4,7 +4,16 @@ import org.contract4j5.controller.Contract4J;
 
 public class SystemUtils {
 	
+	/**
+	 * Get the scripting engine name by first looking for the System property that might exist. If not
+	 * found, then query Contract4J.  The latter is actually more definitive, but if Contract4J hasn't
+	 * been initialized yet, then querying it will implicitly trigger the default initialization process,
+	 * which is probably not desirable!
+	 */
 	public static String getScriptingEngineName() {
+		String name = System.getProperty("interpreter");
+		if (name != null)
+			return name;
 		return Contract4J.getInstance().getContractEnforcer().getExpressionInterpreter().getScriptingEngineName();
 	}
 	
